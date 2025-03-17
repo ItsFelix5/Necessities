6c290843c1c360e953ccad780d1459aa80d2b215
itsfelix5 <felixsoetewey@outlook.com>
2025-03-17 16:40:34 +0100

    Fix lots of things

--- src/main/java/necessities/mixin/ExplosionMixin.java
+++ src/main/java/necessities/mixin/ExplosionMixin.java
@@ -5,19 +5,26 @@ import net.minecraft.particle.ParticleEffect;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.world.World;
 import net.minecraft.world.explosion.Explosion;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Explosion.class)
+@Mixin(value = Explosion.class, priority = 10)
 public class ExplosionMixin {
-    @Redirect(method = "affectWorld", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;addParticle(Lnet/minecraft/particle/ParticleEffect;DDDDDD)V"))
-    public void addParticle(World world, ParticleEffect particle, double x, double y, double z, double velocityX, double velocityY, double velocityZ) {
-        if (!particle.getType().equals(ModParticles.CONFETTI) || !world.isClient) {
-            world.addParticle(particle, x, y, z, velocityX, velocityY, velocityZ);
-            return;
-        }
+    @Shadow @Final private ParticleEffect particle;
+    @Shadow @Final private World world;
+    @Shadow @Final private double x;
+    @Shadow @Final private double y;
+    @Shadow @Final private double z;
+
+    @Inject(method = "affectWorld", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;addParticle(Lnet/minecraft/particle/ParticleEffect;DDDDDD)V"), cancellable = true)
+    public void addParticle(boolean particles, CallbackInfo ci) {
+        if (!particle.getType().equals(ModParticles.CONFETTI) || !world.isClient) return;
         for (int i = 0; i < 15; i++) world.addParticle(ParticleTypes.POOF, x, y, z, world.random.nextGaussian() / 10f, Math.abs(world.random.nextGaussian() / 10f), world.random.nextGaussian() / 10f);
         for (int i = 0; i < 200; i++) world.addParticle(particle, x, y, z, world.random.nextGaussian() / 8f, Math.abs(world.random.nextGaussian() / 8f), world.random.nextGaussian() / 8f);
+        ci.cancel();
     }
 }
---
+++ src/main/java/necessities/mixin/ServerPlayNetworkHandlerMixin.java
@@ -0,0 +1,15 @@
+package necessities.mixin;
+
+import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.server.network.ServerPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(ServerPlayNetworkHandler.class)
+public class ServerPlayNetworkHandlerMixin {
+    @Redirect(method = "onPlayerMove", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerEntity;isInTeleportationState()Z"))
+    private boolean isInTeleportationState(ServerPlayerEntity player) {
+        return true;
+    }
+}
